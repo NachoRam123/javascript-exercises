9131f7b74b1a8c317de9f3c232004cc1bd6273f3
NachoRam123 <nacho_ramella@hotmail.com>
2023-09-27 22:34:37 -0300

    fix agregate operandus in repeatString func

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,11 @@
 const repeatString = function(string, num) {
-    for (i = 1; i<=num; i++) {
-        let text =+ string
+    let text = '' 
+    for (let i = 1; i<=num; i++) {
+        text += string
     }
     return text
 };
-
+// console.log('ajksnckc')
+// console.log(repeatString('akscns', 3))
 // Do not edit below this line
 module.exports = repeatString;
