9281150f4fae1a96fa343d951b69324e04601d52
NachoRam123 <nacho_ramella@hotmail.com>
2023-09-27 22:40:53 -0300

    add error cond for negative num

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,16 @@
 const repeatString = function(string, num) {
-    let text = '' 
-    for (let i = 1; i<=num; i++) {
-        text += string
+    let text = ''
+    if (num>=0) {
+        for (let i = 1; i<=num; i++) {
+            text += string
+        }
+        return text
+    } 
+    else {
+        return 'ERROR'
     }
-    return text
 };
-// console.log('ajksnckc')
+// console.log('ajksnckc')      
 // console.log(repeatString('akscns', 3))
 // Do not edit below this line
 module.exports = repeatString;
