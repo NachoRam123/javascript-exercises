3f69ec90d020435377391cad6aaa3802d81f2aa8
NachoRam123 <nacho_ramella@hotmail.com>
2023-09-27 23:01:07 -0300

    buil reverse func, pass 1st test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+function reverseString (string) {
+    let reverse = '';
+    let large = string.length;
+    for (let i=1; i<=large; i++) {
+        reverse += string[large-i]
+    }
+    return reverse
 };
-
+// let reverseString = 'hola';
+console.log(reverseString('hola'))
 // Do not edit below this line
 module.exports = reverseString;
