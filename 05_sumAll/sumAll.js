ce162c4fc1ef884ee5a14399f9f9ca61c2ca9034
NachoRam123 <nacho_ramella@hotmail.com>
2023-10-15 19:34:01 -0300

    fix negative argmts

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,17 @@
 const sumAll = function(num1, num2) {
-    let suma = 0;
-    var low = num1 < num2 ? num1 : num2;
-    var high = low + Math.abs(num1-num2)
-    for (i = low; i <=high; i++) {
-        suma += i
+    if (num1>=0 & num2>=0) {
+        let suma = 0;
+        var low = num1 < num2 ? num1 : num2;
+        var high = low + Math.abs(num1-num2)
+        for (i = low; i <=high; i++) {
+            suma += i
+        }
+        return suma
+    }    
+    else {
+        return 'ERROR'
     }
-    return suma
-};
-console.log(sumAll(1,3))
+}
+console.log(sumAll(-1,3))
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
