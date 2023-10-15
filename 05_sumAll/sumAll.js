a27f4985bfa59d1c4e25ea031dbdfb9ab372d43a
NachoRam123 <nacho_ramella@hotmail.com>
2023-10-15 20:54:17 -0300

    recovering from git fiasco again

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,5 @@
 const sumAll = function(num1, num2) {
-    if (num1>=0 & num2>=0) {
+    if ((num1>=0 & num2>=0) & (typeof num1 =='number' & typeof num2 =='number')) {
         let suma = 0;
         var low = num1 < num2 ? num1 : num2;
         var high = low + Math.abs(num1-num2)
@@ -12,6 +12,6 @@ const sumAll = function(num1, num2) {
         return 'ERROR'
     }
 }
-console.log(sumAll(-1,3))
+console.log(sumAll(45559,3))
 // Do not edit below this line
 module.exports = sumAll
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if ((year % 4 == 0) && (year % 100 != 0 || (year % 100 == 0 && year % 400 == 0))){
+        return true
+    }
+    else {
+        return false
+    }
 
 };
+// ||year % 4 == 0 || year % 100 != 0))
+console.log(leapYears(2000));
+// const year = 2000;
+// console.log((year % 4 == 0) && (year % 100 != 0 || (year % 100 == 0 && year % 400 == 0)));
+// console.log(year % 100 != 0);
+// console.log(year % 100 != 0 || (year % 100 == 0 && year % 400 == 0))
+
 
 // Do not edit below this line
 module.exports = leapYears;
